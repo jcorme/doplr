0dec41620f91ec0303e32b2ebe3f3d6456f53147
Jason Chen <jason@jcndrop.com>
2020-01-18 21:38:41 -0800

    Add MetadataProvider trait and QueryExpr enum
    The MetadataProvider trait abstracts functionality that all metadata
    provider clients should have. Metadata providers are used during music
    import to match a given file to a artist, release, and track. The trait
    will definitely change, as it will need to provide methods for
    retrieving resources by the provider-specific identifiers, in addition
    to performing search queries.
    
    The trait has to return (boxed) futures for any asynchronous functions
    since async traits aren't supported yet.
    
    The QueryExpr enum is a very basic and very initial attempt to abstract
    elements of search queries common to all metadata providers. The syntax
    is pretty clunky, and the whole type may turn out to be necessary, but
    it's a first step.

--- src/metadata/providers/mod.rs
+++ src/metadata/providers/mod.rs
@@ -1,1 +1,40 @@
+use std::pin::Pin;
+
 pub mod musicbrainz;
+
+pub enum QueryExpr<'a> {
+    Title(&'a str),
+    Artist(&'a str),
+    Artists(Vec<&'a str>),
+    Album(&'a str),
+    Year(u16),
+    TrackNumber(u16),
+    TotalTracks(u16),
+    Length(u32),
+    // Key => Value
+    Custom(&'a str, &'a str),
+    AND(Box<QueryExpr<'a>>, Box<QueryExpr<'a>>),
+    OR(Box<QueryExpr<'a>>, Box<QueryExpr<'a>>),
+}
+
+impl<'a> QueryExpr<'a> {
+    pub fn and(self, other: QueryExpr<'a>) -> QueryExpr<'a> {
+        QueryExpr::AND(Box::new(self), Box::new(other))
+    }
+
+    pub fn or(self, other: QueryExpr<'a>) -> QueryExpr<'a> {
+        QueryExpr::OR(Box::new(self), Box::new(other))
+    }
+}
+
+pub trait MetadataProvider {
+    type Response: std::fmt::Debug;
+    type Error: std::error::Error;
+
+    // Until async traits are merged, must return a future here rather than
+    // make the function async. No way to avoid the extra allocation(s).
+    fn run_query<'a>(&self, q: QueryExpr<'a>)
+        -> Pin<Box<dyn std::future::Future<Output = Result<Self::Response, Self::Error>> + Send + 'a>>;
+
+    fn build_query<'a>(q: QueryExpr<'a>) -> String;
+}
